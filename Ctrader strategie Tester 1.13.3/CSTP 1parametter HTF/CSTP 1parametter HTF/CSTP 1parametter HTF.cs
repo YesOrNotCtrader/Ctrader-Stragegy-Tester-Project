a5ab28c377f7159da1480b099c284e2211750c97
YesOrNotCtrader <algonin01010@gmail.com>
2025-05-05 19:02:43 +0200

    Add files via upload

---
+++ Ctrader strategie Tester 1.13.3/CSTP 1parametter HTF/CSTP 1parametter HTF/CSTP 1parametter HTF.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using cAlgo.API;
+using cAlgo.API.Collections;
+using cAlgo.API.Indicators;
+using cAlgo.API.Internals;
+
+namespace cAlgo
+{
+    [Cloud("Result", "Signal", FirstColor = "Lime", SecondColor = "Red", Opacity = 0.25)]
+    [Indicator(IsOverlay = false, AccessRights = AccessRights.None)]
+    public class CSTP_Indi_1ParametterHTF : Indicator
+    {
+        [Parameter("TimeFrame", DefaultValue = "Daily", Group = "Calculation Type")]
+        public TimeFrame TF1 { get; set; }
+
+        [Parameter(DefaultValue = CSTP_Indi_1Parametter.EnumIndiSelection.Exponetial_Moving_Average)]
+        public CSTP_Indi_1Parametter.EnumIndiSelection IndicatorSelection { get; set; }
+
+        [Parameter(DefaultValue = 13)]
+        public int Period { get; set; }
+
+        [Output("Result", LineColor = "Green", LineStyle = LineStyle.Solid, PlotType = PlotType.Line, Thickness = 1)]
+        public IndicatorDataSeries Result { get; set; }
+        [Output("Signal", LineColor = "Red", LineStyle = LineStyle.Solid, PlotType = PlotType.Line, Thickness = 1)]
+        public IndicatorDataSeries Signal { get; set; }
+
+        private CSTP_Indi_1Parametter cstp;
+        private Bars bars1;
+
+        protected override void Initialize()
+        {
+            bars1 = MarketData.GetBars(TF1);
+
+            if (!IsBacktesting)
+            {
+                while (bars1.OpenTimes[0] > Bars.OpenTimes[0])
+                    bars1.LoadMoreHistory();
+            }
+            cstp = Indicators.GetIndicator<CSTP_Indi_1Parametter>(bars1, IndicatorSelection, Period);
+        }
+
+        public override void Calculate(int index)
+        {
+            int idx1 = TF1 == Chart.TimeFrame ? index : bars1.OpenTimes.GetIndexByTime(Bars.OpenTimes[index]);
+
+            Result[index] = cstp.Result[idx1];
+
+            Signal[index] = cstp.Signal[idx1];
+
+        }
+    }
+}
